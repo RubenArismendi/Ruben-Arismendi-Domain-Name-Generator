6e7720380f9154667bf8d86af425f9b9a0fdedfc
RubenArismendi <rubenaugustoarismendi@gmail.com>
2023-06-30 18:03:46 +0000

    version 2

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["the", "our", "their"];
+  let adjetivo = ["great", "big", "awesome"]; //arrays
+  let sustantivo = ["jogger", "racoon", "capybara"];
+
+  for (let i = 0; i < pronombre.length; i++) {
+    //for anidados
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let e = 0; e < sustantivo.length; e++) {
+        console.log(`${pronombre[i]}${adjetivo[a]}${sustantivo[e]}.com`); //resultado de cada bucle for al final + concatenacion
+      }
+    }
+  }
 };
